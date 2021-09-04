1c48125c08e42f562dbc869e3e446a6d14ff5c3a
Ocete <ocetebt@hotmail.com>
2021-09-04 12:37:17 +0200

    Adding introduction, unreviewed

--- thesis/bib/prepare_bib.py
+++ thesis/bib/prepare_bib.py
@@ -14,6 +14,18 @@ filedata = filedata.replace('{~}', '~')
 filedata = filedata.replace('{\^}', '^')
 filedata = filedata.replace('{\"}', '"')
 
+# Add not-found citation in Mendeley manually
+if 'Manin1980' not in filedata:
+  filedata += """
+  @article{Manin1980,
+    title={Computable and uncomputable},
+    author={Manin, Yuri},
+    journal={Sovetskoye Radio, Moscow},
+    volume={128},
+    year={1980}
+  }
+  """
+
 # Write the file out again
 with open(filename, 'w') as file:
 	file.write(filedata)
